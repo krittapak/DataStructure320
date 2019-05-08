9a5eb78a0a03e3cd613c7161cfbaf23ab3eb9e49
krittapak <be.me@outlook.com>
2019-05-08 20:37:16 +0700

    Add Friday Test 2

---
+++ src/Test2/BinarySearch.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Test2;
+
+/**
+ *
+ * @author KRITTAPAK
+ */
+
+
+//จาก BinarySearchTree class ซึ่งใช้ array เก็บข้อมูลอยู่เบื้องหลัง และใช้ Comparator 
+//ที่กำหนดให้ในการจัดลำดับข้อมูล โดยข้อมูลตัวแรกที่ใส่เข้าไปใน BinarySearchTree นี้ 
+//จะใส่ที่ช่องแรกของ array ซึ่งเป็น root node ของ BinarySearchTree นี้เสมอ และ array 
+//ช่องที่มีค่าเป็น null คือ ช่องที่ไม่มี node ของ tree จงเขียน 
+//1. method สร้าง ArrayList ที่ประกอบด้วยสมาชิกที่เกิดการจากท่องไปใน tree แบบ in-order
+//2. method สร้าง ArrayList ที่ประกอบด้วยสมาชิกที่เกิดการจากท่องไปใน tree แบบ pre-order
+//3. method สร้าง ArrayList ที่ประกอบด้วยสมาชิกที่เกิดการจากท่องไปใน tree แบบ post-order
+public class BinarySearch {
+    
+}
