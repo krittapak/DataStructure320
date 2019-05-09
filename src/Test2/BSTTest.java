6fb495969a712de965234d454e9d957c8074b613
krittapak <be.me@outlook.com>
2019-05-09 12:12:43 +0700

    Add Exam Project

---
+++ src/Test2/BSTTest.java
@@ -0,0 +1,59 @@
+
+import binarysearchtree.BinarySearchTree;
+import java.util.Random;
+
+public class BSTTest {
+
+    public static void main(String[] args) {
+        binarySearchTreeToString();
+    }
+
+    private static void binarySearchTreeToString() {
+
+        BinarySearchTree<X> bst = new BinarySearchTree<>();
+        bst.add(new X(7));
+        bst.add(new X(2));
+        bst.add(new X(11));
+        bst.add(new X(1));
+        bst.add(new X(6));
+        bst.add(new X(8));
+        bst.add(new X(12));
+        System.out.println(bst);
+
+    }
+
+    private static X randomX() {
+        Random rand = new Random();
+        return new X(rand.nextInt(900) + 100);
+    }
+}
+
+class X implements Comparable<X> {
+
+    private final int id;
+    private final int seq;
+    private static int next;
+
+    public X(int id) {
+        this.id = id;
+        this.seq = next++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getSeq() {
+        return seq;
+    }
+
+    @Override
+    public int compareTo(X x) {
+        return this.id - x.id;
+    }
+
+    @Override
+    public String toString() {
+        return "X[" + id + "(" + seq + ")]";
+    }
+}
